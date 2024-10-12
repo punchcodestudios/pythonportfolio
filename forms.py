4de7087ee105c6af85b5270fd7af467d4dc897b3
Patrick Schandler <pschandler@gmail.com>
2024-10-12 08:41:02 -0500

    removing email validator for now until successful deploy

--- forms.py
+++ forms.py
@@ -8,6 +8,6 @@ from flask_ckeditor import CKEditorField
 
 class ContactForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired(), Email()])
+    email = StringField('Email', validators=[DataRequired()])
     message = TextAreaField('Message', validators=[DataRequired(), Length(max=1000)])
     submit = SubmitField('Submit')
