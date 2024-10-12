ee5316b8232af849e5c1a58feee295e9f731abf9
Patrick Schandler <pschandler@gmail.com>
2024-10-12 09:50:38 -0500

    updating with database key

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ Bootstrap5(app)
 # CREATE DATABASE
 class Base(DeclarativeBase):
     pass
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///python-portfolio.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URI",  'sqlite:///python-portfolio.db')
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
