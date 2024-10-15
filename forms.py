bfcb2542f8fb390e41776572b57f9b18309c5f7d
Patrick Schandler <pschandler@gmail.com>
2024-10-15 14:00:19 -0500

    adding cafe project

--- forms.py
+++ forms.py
@@ -1,13 +1,35 @@
-
+from tkinter.ttk import Button
 
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField
+from wtforms.fields.choices import SelectField
 from wtforms.fields.simple import TextAreaField
 from wtforms.validators import DataRequired, URL, Email, Length
+from wtforms.validators import DataRequired, ValidationError
 from flask_ckeditor import CKEditorField
+
+
+def validate_url(form, field):
+    valid = field.data.split('://')[0] == 'https'
+    if not valid:
+        raise ValidationError('value is missing required "https"')
+    return valid
 
 class ContactForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired()])
     message = TextAreaField('Message', validators=[DataRequired(), Length(max=1000)])
     submit = SubmitField('Submit')
+
+class CoffeeShopForm(FlaskForm):
+    cafe_name = StringField('Cafe Name', validators=[DataRequired()])
+    location = StringField('Location', validators=[DataRequired()])
+    URL = StringField('URL', validators=[DataRequired(), validate_url])
+    open_time = StringField('Open', validators=[DataRequired()])
+    closing_time = StringField('Close', validators=[DataRequired()])
+    coffee_rating = SelectField("Coffee Rating", choices=["☕️", "☕☕", "☕☕☕", "☕☕☕☕", "☕☕☕☕☕"],
+                                validators=[DataRequired()])
+    wifi_rating = SelectField("Wifi Strength Rating", choices=["✘", "💪", "💪💪", "💪💪💪", "💪💪💪💪", "💪💪💪💪💪"], validators=[DataRequired()])
+    power_outlet_rating = SelectField("Power Socket Availability", choices=["✘", "🔌", "🔌🔌", "🔌🔌🔌", "🔌🔌🔌🔌", "🔌🔌🔌🔌🔌"], validators=[DataRequired()])
+    submit = SubmitField('Submit')
+    cancel = SubmitField('Cancel')
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-
+import csv
 from flask import Flask, render_template, flash, redirect, url_for
 from flask_bootstrap import Bootstrap5
 from flask_sqlalchemy import SQLAlchemy
@@ -11,16 +11,17 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired, URL
 from flask_ckeditor import CKEditor, CKEditorField
 from datetime import date
-from forms import ContactForm
+from forms import ContactForm, CoffeeShopForm
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get('FLASK_KEY') #''
+app.config['SECRET_KEY'] = os.environ.get('FLASK_KEY', '8BYkEfBA6O6donzWlSihBXox7C0sKR6b') #''
 ckEditor = CKEditor(app)
 Bootstrap5(app)
 
 # CREATE DATABASE
 class Base(DeclarativeBase):
     pass
+
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URI",  'sqlite:///python-portfolio.db')
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
@@ -71,6 +72,46 @@ def projects():
         'header_img': 'projects.png'
     }
     return render_template("pages/projects.html", **context)
+
+@app.route('/projects/cafes')
+def cafes():
+    list_of_rows = []
+    titles = ["Cafe Name", "Location", "Open", "Close", "Coffee", "Wifi", "Power"]
+    context = {
+        'header_img': 'projects.png',
+        'cafes': list_of_rows,
+        'titles': titles
+    }
+    with open('instance/cafe-data.csv', newline='', encoding='utf-8') as csv_file:
+        csv_data = csv.reader(csv_file, delimiter=',')
+        for row in csv_data:
+            list_of_rows.append(row)
+
+    return render_template('pages/cafes.html', **context)
+
+@app.route('/projects/add_cafe', methods=['GET', 'POST'])
+def add_cafe():
+    form = CoffeeShopForm()
+    context = {
+        'header_img': 'projects.png',
+        "form": form
+    }
+    if form.cancel.data:
+        return redirect(url_for('cafes'))
+
+    if form.validate_on_submit():
+        with open("instance/cafe-data.csv", mode="a", encoding='utf-8') as csv_file:
+            csv_file.write(f"\n{form.cafe_name.data},"
+                           f"{form.location.data},"
+                           f"{form.open_time.data},"
+                           f"{form.closing_time.data},"
+                           f"{form.coffee_rating.data},"
+                           f"{form.wifi_rating.data},"
+                           f"{form.power_outlet_rating.data}")
+        return redirect(url_for('cafes'))
+    # Exercise:
+    # Make the form write a new row into cafe-data.csv
+    return render_template('pages/add_cafe.html', **context)
 
 if __name__ == "__main__":
     app.run(debug=True, port=5003)
